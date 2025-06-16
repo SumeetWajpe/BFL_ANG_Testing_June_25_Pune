770f128c99241828fe98eb94925c62cc3b55cb8f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-16 15:47:57 +0530

    ✨ using async/await with resolves

--- DAYI/JESTBasics/src/async/axios.fetchdata.spec.js
+++ DAYI/JESTBasics/src/async/axios.fetchdata.spec.js
@@ -21,7 +21,7 @@ describe("test suite for testing async code with promises & async/await", () => 
     });
   });
 
-  fit("tests for async code (using async/await) that fetchPosts returns", async () => {
+  it("tests for async code (using async/await) that fetchPosts returns", async () => {
     try {
       const post = await postsModule.fetchPostWithId(1);
       expect(post.userId).toBe(2);
@@ -29,5 +29,28 @@ describe("test suite for testing async code with promises & async/await", () => 
       console.log(error);
       throw error; // Re-throw the error to fail the test if needed
     }
+  });
+
+  it("tests for async code using promise with method (resolves) siven by JEST", () => {
+    var mockResult = {
+      userId: 2,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    };
+
+    return expect(postsModule.fetchPostWithId(1)).resolves.toEqual(mockResult);
+  });
+  fit("tests for async code using promise with method (resolves) siven by JEST", async () => {
+    var mockResult = {
+      userId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    };
+
+    await expect(postsModule.fetchPostWithId(1)).resolves.toEqual(mockResult);
   });
 });
