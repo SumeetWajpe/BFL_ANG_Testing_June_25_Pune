29606f1d5479494923d3898b9d9192a6e3d9c707
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-17 11:59:20 +0530

    ✨ testing if title property of App Component is equal to pokedex-angular

---
+++ DAY2/pokedex/src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { LoaderService } from "./shared/services/loader.service";
+import { RouterTestingModule } from "@angular/router/testing";
+describe("test suite for testing App Component", () => {
+  it("should create App Component Instance ", async () => {
+    // arrange
+    // configure the testing module
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      providers: [LoaderService],
+      imports: [RouterTestingModule],
+    }).compileComponents(); // compile the css & html
+
+    // act
+    // create the component instance
+    const fixture = TestBed.createComponent(AppComponent);
+    const compInstance = fixture.componentInstance;
+
+    // assert
+    // check if the component instance is created successfully
+    expect(compInstance).toBeTruthy();
+  });
+  it("should test for title property of App Component to be podex-angular", async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      providers: [LoaderService],
+      imports: [RouterTestingModule],
+    }).compileComponents(); // compile the css & html
+
+    // act
+    // create the component instance
+    const fixture = TestBed.createComponent(AppComponent);
+    const compInstance = fixture.componentInstance;
+    expect(compInstance.title).toBe("pokedex-angular");
+  });
+});
