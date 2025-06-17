fbd99942d5bb2b777cf59acd8ad1a0a3c290c881
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-17 12:01:17 +0530

    ✨ using beforeEach()

--- DAY2/pokedex/src/app/app.component.spec.ts
+++ DAY2/pokedex/src/app/app.component.spec.ts
@@ -3,16 +3,15 @@ import { AppComponent } from "./app.component";
 import { LoaderService } from "./shared/services/loader.service";
 import { RouterTestingModule } from "@angular/router/testing";
 describe("test suite for testing App Component", () => {
-  it("should create App Component Instance ", async () => {
-    // arrange
-    // configure the testing module
+  beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       providers: [LoaderService],
       imports: [RouterTestingModule],
     }).compileComponents(); // compile the css & html
-
-    // act
+  });
+  it("should create App Component Instance ", () => {
+     // act
     // create the component instance
     const fixture = TestBed.createComponent(AppComponent);
     const compInstance = fixture.componentInstance;
@@ -21,13 +20,7 @@ describe("test suite for testing App Component", () => {
     // check if the component instance is created successfully
     expect(compInstance).toBeTruthy();
   });
-  it("should test for title property of App Component to be podex-angular", async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      providers: [LoaderService],
-      imports: [RouterTestingModule],
-    }).compileComponents(); // compile the css & html
-
+  it("should test for title property of App Component to be podex-angular", () => {
     // act
     // create the component instance
     const fixture = TestBed.createComponent(AppComponent);
