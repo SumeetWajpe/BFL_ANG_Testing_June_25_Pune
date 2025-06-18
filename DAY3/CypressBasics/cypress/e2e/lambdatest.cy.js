379f98028b16a4ed2ae5a24adf942e1dfc740dc8
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-18 15:37:52 +0530

    ✨ using fixture to load the data

--- DAY3/CypressBasics/cypress/e2e/lambdatest.cy.js
+++ DAY3/CypressBasics/cypress/e2e/lambdatest.cy.js
@@ -1,5 +1,5 @@
 describe("test suite for lambdatest.io", () => {
-  it("should login successfully", () => {
+  xit("should login successfully", () => {
     cy.visit(
       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
     );
@@ -10,5 +10,15 @@ describe("test suite for lambdatest.io", () => {
       "eq",
       "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
     );
+  });
+
+  it("uses fixture to load the data", () => {
+    cy.fixture("login.json").then(data => {
+      cy.visit(data.loginUrl);
+      cy.get("#input-email").type(data.email);
+      cy.get("#input-password").type(data.pwd);
+      cy.get("form > .btn").click();
+      cy.url().should("eq", data.loginSuccessUrl);
+    });
   });
 });
