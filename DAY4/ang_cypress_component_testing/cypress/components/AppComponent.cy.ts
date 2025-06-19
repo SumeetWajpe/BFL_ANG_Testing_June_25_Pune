8fd0d6ec9b02411d6c61bbe70381d4c53577af15
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-19 14:18:08 +0530

    ✨ Pass component properties

--- DAY4/ang_cypress_component_testing/cypress/components/AppComponent.cy.ts
+++ DAY4/ang_cypress_component_testing/cypress/components/AppComponent.cy.ts
@@ -3,6 +3,21 @@ import { AppComponent } from '../../src/app/app.component';
 describe('test suite for AppComponent', () => {
   it('should instatiate (mount) the app component', () => {
     // 1st way
-    cy.mount(AppComponent); // instantiates and mounts the AppComponent in the browser
+    // cy.mount(AppComponent); // instantiates and mounts the AppComponent in the browser
+    // 2nd way
+    // cy.mount(AppComponent, {
+    //   declarations: [AppComponent],
+    //   imports: [],
+    //   providers: [],
+    // });
+    // cy.get('h1').should('contain', 'Hello, ang_cypress_component_testing');
+
+    // 3rd Way
+    cy.mount(AppComponent, {
+      componentProperties: {
+        title: 'Dynamic Title',
+      },
+    });
+    cy.get('h1').should('contain', 'Dynamic Title');
   });
 });
